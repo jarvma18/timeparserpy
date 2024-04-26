34eef37f08304e3ff095c0653bf5527bb39a7b12
jarvma18 <markusmjarvinen@outlook.com>
2024-04-27 01:08:56 +0300

    Added new test parameters.

--- test.py
+++ test.py
@@ -4,12 +4,11 @@ from main import time_parser
 
 class TestTimeParser(unittest.TestCase):
   def test_time_parser(self):
-    self.assertEqual(time_parser('12:00:00'), 720)
-    self.assertEqual(time_parser('00:00:00'), 0)
-    self.assertEqual(time_parser('23:59:59'), 1439)
-    self.assertEqual(time_parser('01:00:00'), 60)
-    self.assertEqual(time_parser('00:01:00'), 1)
-    self.assertEqual(time_parser('00:00:01'), 0)
+    self.assertEqual(time_parser('4'), 16 * 60)
+    self.assertEqual(time_parser('7:38pm'), 19 * 60 + 38)
+    self.assertEqual(time_parser('23:42'), 23 * 60 + 42)
+    self.assertEqual(time_parser('3:16'), 3 * 60 + 16)
+    self.assertEqual(time_parser('3:16am'), 3 * 60 + 16)
 
 if __name__ == '__main__':
   unittest.main()
