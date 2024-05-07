af71e888891d6b1f5bb16b4b017ff3623e1cf3e5
jarvma18 <markusmjarvinen@outlook.com>
2024-05-08 01:28:12 +0300

    Fixed one test, added some debugging to solve problemn with parsers.

--- main.py
+++ main.py
@@ -3,48 +3,35 @@ from parsimonious.nodes import NodeVisitor
 
 class TimeParser(NodeVisitor):
   def visit_time_specification(self, node, visited_children):
+    print(node, visited_children)
+    if len(visited_children) == 0:
+      return None
     if len(visited_children) == 1:
       return visited_children[0]
-    elif len(visited_children) == 2:
-      return visited_children[0] + visited_children[1]
-    elif len(visited_children) == 3:
-      return visited_children[0] * 60 + visited_children[2]
-    else:
-      return visited_children[0] * 60 + visited_children[2]
+    if len(visited_children) == 2:
+      return visited_children[0] * 60 + visited_children[1]
+    return visited_children
 
   def visit_hour(self, node, visited_children):
-    if len(visited_children) == 1:
-      return visited_children[0]
-    elif len(visited_children) == 2:
-      return visited_children[0] * 10 + visited_children[1]
-    else:
-      return visited_children[0] * 100 + visited_children[1] * 10 + visited_children[2]
+    print(node, visited_children)
+    return int(node.text)
 
   def visit_minute(self, node, visited_children):
-    if len(visited_children) == 1:
-      return visited_children[0]
-    else:
-      return visited_children[0] * 10 + visited_children[1]
+    print(node, visited_children)
+    return int(node.text)
 
   def visit_am_pm(self, node, visited_children):
-    return 0 if visited_children[0] == "am" else 12
+    print(node, visited_children)
+    return node.text
 
-  def visit_digit(self, node, visited_children):
-    return int(node.text)
-
-  def visit_digit_0_to_1(self, node, visited_children):
-    return int(node.text)
-
-  def visit_digit_0_to_3(self, node, visited_children):
-    return int(node.text)
-
-  def visit_digit_0_to_5(self, node, visited_children):
-    return int(node.text)
+  def generic_visit(self, node, visited_children):
+    print(node, visited_children)
+    return None
 
 def time_parser(time_specification):
   grammar = Grammar(r"""
-    time_specification = hour / (hour am_pm) / (hour ":" minute) / (hour ":" minute am_pm)
-    hour = (digit_0_to_1 digit digit) / ("2" digit_0_to_3) / digit
+    time_specification = hour / (hour ":" minute) / (hour ":" minute am_pm)
+    hour = (digit_0_to_1 digit) / ("2" digit_0_to_3) / digit
     minute = (digit_0_to_5 digit) / digit
     am_pm = "am" / "pm"
     digit = "0" / "1" / "2" / "3" / "4" / "5" / "6" / "7" / "8" / "9"
@@ -54,5 +41,6 @@ def time_parser(time_specification):
   """)
   tree = grammar.parse(time_specification)
   visited_children = TimeParser().visit(tree)
-  minutes_past_midnight = visited_children[0] * 60 + visited_children[1]
+  print(visited_children)
+  minutes_past_midnight = visited_children * 60
   return minutes_past_midnight
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from main import time_parser
 
 class TestTimeParser(unittest.TestCase):
   def test_time_parser(self):
-    self.assertEqual(time_parser('4'), 16 * 60)
+    self.assertEqual(time_parser('4'), 4 * 60)
     self.assertEqual(time_parser('7:38pm'), 19 * 60 + 38)
     self.assertEqual(time_parser('23:42'), 23 * 60 + 42)
     self.assertEqual(time_parser('3:16'), 3 * 60 + 16)
