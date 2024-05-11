f489a9099756ba97abff4135a27aa1ed40b1cee5
jarvma18 <markusmjarvinen@outlook.com>
2024-05-11 14:51:40 +0300

    Now the parser seems to work and does not get stuck to some values, reason for this was that in PEG parser you must order the most complicated pattern first so it does not get stuck with match to first simple pattern without matching to end of the sequence, this was something the copilot did not understand to do.

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor
 
 grammar = Grammar(
-  r"""
-  
+  """
+  time = (hour ":" minute am_pm) / (hour am_pm) / (hour  ":" minute) / hour
+  hour = (digit_2 digit_0_4) / (digit_0_1 digit) / digit
+  minute = digit_0_5 digit
+  digit = ~"[0-9]"
+  digit_0_1 = ~"[0-1]"
+  digit_2 = "2"
+  digit_0_4 = ~"[0-4]"
+  digit_0_5 = ~"[0-5]"
+  am_pm = "am" / "pm"
   """
 )
+
+def time_parser(time: str):
+  print(grammar.parse(time))
+  return
+
+def main():
+  time_parser('15')
+
+if __name__ == "__main__":
+  main()
