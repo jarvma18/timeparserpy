7d8abceb7c552c2cfab135f3faa60334a93873cd
jarvma18 <markusmjarvinen@outlook.com>
2024-05-12 13:52:03 +0300

    Removed prints, fixed grammar a bit, added some visit method to TimeParser class that was missing.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from parsimonious.nodes import NodeVisitor
 grammar = Grammar(
   """
   time = (hour ":" minute am_pm) / (hour am_pm) / (hour  ":" minute) / hour
-  hour = (digit_2 digit_0_4) / (digit_0_1 digit) / digit
+  hour = (digit_2 digit_0_3) / (digit_0_1 digit) / digit
   minute = digit_0_5 digit
   digit = ~"[0-9]"
   digit_0_1 = ~"[0-1]"
   digit_2 = "2"
-  digit_0_4 = ~"[0-4]"
+  digit_0_3 = ~"[0-3]"
   digit_0_5 = ~"[0-5]"
   am_pm = "am" / "pm"
   """
@@ -23,8 +23,6 @@ class TimeParser(NodeVisitor):
     self.am_pm = None
 
   def visit_time(self, node, visited_children):
-    print(node.text)
-    print('visittime', visited_children)
     visited_children = visited_children[0]
     if type(visited_children) == int:
       self.hour = visited_children
@@ -37,11 +35,9 @@ class TimeParser(NodeVisitor):
       self.am_pm = visited_children[3]
 
   def visit_hour(self, node, visited_children):
-    print('visithour', visited_children)
     return int(node.text)
 
   def visit_minute(self, node, visited_children):
-    print('visitmin', visited_children)
     return int(node.text)
 
   def visit_am_pm(self, node, visited_children):
@@ -53,12 +49,20 @@ class TimeParser(NodeVisitor):
   def visit_digit_0_to_3(self, node, visited_children):
     return node.text
 
+  def visit_digit_0_to_1(self, node, visited_children):
+    return node.text
+
+  def visit_digit_2(self, node, visited_children):
+    return node.text
+
+  def visit_digit_0_to_5(self, node, visited_children):
+    return node.text
+
   def generic_visit(self, node, visited_children):
     return visited_children or node
 
   def parse(self):
     tree = grammar.parse(self.text)
-    print(tree)
     self.visit(tree)
     return self.hour, self.minute, self.am_pm
 
@@ -66,9 +70,6 @@ def time_parser(text):
   parser = TimeParser(text)
   minutesPastMidnight: int = None
   hour, minute, am_pm = parser.parse()
-  print('hour', hour)
-  print('minute', minute)
-  print('am_pm', am_pm)
   if type(hour) == int:
     if type(am_pm) == str and am_pm == 'pm':
       minutesPastMidnight = (hour + 12) * 60
