6c71b55fed1dd3c0afd8983bb0a15f0f991bfdea
jarvma18 <markusmjarvinen@outlook.com>
2024-05-19 01:04:57 +0300

    Added some more test cases.

--- test.py
+++ test.py
@@ -25,15 +25,30 @@ class TestTimeParser(unittest.TestCase):
         else:
           self.assertEqual(time_parser(f'{i}:{j}'), i * 60 + j)
 
+  def test_time_parser_single_hour_am_pm(self):
+    for i in range(1, 12):
+      self.assertEqual(time_parser(f'{i}am'), i * 60)
+      self.assertEqual(time_parser(f'{i}pm'), i * 60 + 12 * 60)
+
+  def test_time_parser_hour_and_minute_am_pm(self):
+    for i in range(1, 12):
+      for j in range(1, 59):
+        if j < 10:
+          self.assertEqual(time_parser(f'{i}:0{j}am'), i * 60 + j)
+          self.assertEqual(time_parser(f'{i}:0{j}pm'), i * 60 + j + 12 * 60)
+        else:
+          self.assertEqual(time_parser(f'{i}:{j}am'), i * 60 + j)
+          self.assertEqual(time_parser(f'{i}:{j}pm'), i * 60 + j + 12 * 60)
+
   def test_time_parser_should_not_parse_over_12_when_using_am_pm(self):
     with self.assertRaises(IncompleteParseError):
       time_parser('13:00am')
 
-  def test_time_parser_should_not_parse_over_12_when_using_am_pm(self):
+  def test_time_parser_rise_parse_error(self):
     with self.assertRaises(ParseError):
       time_parser('lölölölöl')
 
-  def test_time_parser_invalid(self):
+  def test_time_parser_raisa_incomplete_parse_error(self):
     with self.assertRaises(IncompleteParseError):
       time_parser('10010101')
 
@@ -41,6 +56,28 @@ class TestTimeParser(unittest.TestCase):
     for i in range(0, 23):
       for j in range(0, 59):
         self.assertEqual(get_minutes_past_midnight(i, j, ''), i * 60 + j)
+
+  def test_get_minutes_past_midnight_single_hour(self):
+    for i in range(0, 23):
+      self.assertEqual(get_minutes_past_midnight(i, None, None), i * 60)
+
+  def test_get_minutes_past_midnight_single_hour_am(self):
+    for i in range(1, 12):
+      self.assertEqual(get_minutes_past_midnight(i, None, 'am'), i * 60)
+
+  def test_get_minutes_past_midnight_single_hour_pm(self):
+    for i in range(1, 12):
+      self.assertEqual(get_minutes_past_midnight(i, None, 'pm'), i * 60 + 12 * 60)
+
+  def test_get_minutes_past_midnight_am(self):
+    for i in range(1, 12):
+      for j in range(0, 59):
+        self.assertEqual(get_minutes_past_midnight(i, j, 'am'), i * 60 + j)
+
+  def test_get_minutes_past_midnight_pm(self):
+    for i in range(1, 12):
+      for j in range(0, 59):
+        self.assertEqual(get_minutes_past_midnight(i, j, 'pm'), i * 60 + j + 12 * 60)
 
 if __name__ == '__main__':
   unittest.main()
